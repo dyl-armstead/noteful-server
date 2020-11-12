fa977da4cadf6dad3aa2694495d08520e627a57d
dyl-armstead <67605274+dyl-armstead@users.noreply.github.com>
2020-11-12 10:09:52 -0800

    initial upload

---
+++ seeds/seed.notes.sql
@@ -0,0 +1,6 @@
+INSERT INTO folders (id, title, content, date_modified)
+VALUES
+  (1,  "Assignments", "Random assignments, So many assignments", 2016-06-22),
+  (2,  "Lessons", "React and Postgres", 2016-07-22),
+  (3,  "Work Memo", "Thanksgiving is a 4 day weekend", 2016-11-22),
+  (4,  "Grocery List", "Pick up bread from the store", 2016-12-22);
